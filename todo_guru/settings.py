2e9ea32c1f29f635525940cea56268d6f6e0562b
Oscar Sabet <osabet021@gmail.com>
2025-03-25 12:02:22 +0000

    update readme

--- todo_guru/settings.py
+++ todo_guru/settings.py
@@ -101,12 +101,6 @@ WSGI_APPLICATION = "todo_guru.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 
 
 # Password validation
